9d95c9ffcb36f45f6b202828c4b7db9ec7b59398
Ivan Shynkarenka <chronoxor@gmail.com>
2022-11-25 01:59:55 +0400

    Migrate to Visual Studio 2022

--- setup/version.h
+++ setup/version.h
@@ -1,9 +1,9 @@
 
-#define VLDVERSION          L"2.5.3"
-#define VERSION_NUMBER		2,5,3,0
-#define VERSION_STRING		"2.5.3.0"
-#define VERSION_COPYRIGHT	"Copyright (C) 2005-2019"
+#define VLDVERSION          L"2.5.4"
+#define VERSION_NUMBER      2,5,4,0
+#define VERSION_STRING      "2.5.4.0"
+#define VERSION_COPYRIGHT   "Copyright (C) 2005-2022"
 
 #ifndef __FILE__
-!define VLD_VERSION "2.5.3"	// NSIS Script
+!define VLD_VERSION "2.5.4" // NSIS Script
 #endif
